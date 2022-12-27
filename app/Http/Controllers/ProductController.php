1b7ef7b0e0668e592f34d8db4faed469f8fee982
smr1897 <sahan.m.rajapaksha@gmail.com>
2022-12-27 23:31:14 +0530

    added new files and made some changes in the exsisting files

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //validate the input
+            $request->validate([
+                'name'=>'required',
+                'detail'=>'required'
+            ]);
+
+        //create new product in the database
+            product::create($request->all());
+
+        //redirect the user and send friendly message
+            return redirect()->route('products.index')->with('success','product created sucessfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(product $product)
+    {
+        //
+    }
+}
---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,62 @@
+@extends('products.layout')
+
+{{-- @section('content')
+    <p>hgvjuhyg</p>
+@endsection
+
+@section('sidebar')
+    @parent
+    <h3>lkjihlki</h3>
+@endsection     --}}
+
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+
+        <div class="pull-left">
+            <h2>Add new Product</h2>
+        </div>
+
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{route('products.index')}}">Back</a>
+        </div>
+
+    </div>
+
+</div>
+
+@if ($errors->any()))
+    <div class="alert alert-danger">
+        <strong>Whoops!!</strong>Ther are some  problems with your input <br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+
+                <li>
+                    {{$error}}
+                </li>
+                
+            @endforeach
+        </ul>
+    </div>
+    
+@endif
+
+<form action="{{route('products.store')}}" method="POST">
+    @csrf
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name : </strong>
+                <input type="text" name="name" class="form-control" placeholder="name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Detail : </strong>
+                <textarea class="form-control" name="detail" placeholder="detail" style="height: 150px"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>    
+</form>
